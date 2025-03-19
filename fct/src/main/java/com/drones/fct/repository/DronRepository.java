14b29818fa54f7e7c06beb31813e19166d7c50a7
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-03-19 07:43:39 +0100

    Renombrada la entidad Dron a Drone y añadidos métodos para buscar drones por coordenadas y matriz en la clase repository correspondiente

--- fct/src/main/java/com/drones/fct/repository/DronRepository.java
+++ fct/src/main/java/com/drones/fct/repository/DronRepository.java
@@ -1,9 +1,13 @@
 package com.drones.fct.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.drones.fct.domain.Dron;
+import com.drones.fct.domain.Drone;
 
-public interface DronRepository extends JpaRepository<Dron, Long> {
+public interface DronRepository extends JpaRepository<Drone, Long> {
+  List<Drone> findByXAndMatrixId(Integer x, Integer y, Long matrixId);
 
+  List<Drone> findByMatrixId(Long matrixId);
 }
