ec382dfc54df48439c8e2f8c830b6c73140d3058
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-03-19 09:51:01 +0100

    Añadida la clase CreateMatrixRequest para gestionar la creación de matrices con validaciones de entrada

---
+++ fct/src/main/java/com/drones/fct/dto/CreateMatrixRequest.java
@@ -0,0 +1,21 @@
+package com.drones.fct.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateMatrixRequest {
+
+  @NotNull
+  @Min(1)
+  @Schema(description = "Maximum value of the X coordinate")
+  private Integer maxX;
+
+  @NotNull
+  @Min(1)
+  @Schema(description = "Maximum value of the Y coordinate")
+  private Integer maxY;
+
+}
