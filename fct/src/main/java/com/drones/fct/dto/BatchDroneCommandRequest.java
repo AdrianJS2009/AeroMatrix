2c1b1e933b463fe4873b320af3b052491f3e29e2
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-03-19 11:58:08 +0100

    Añadida la clase BatchDroneCommandRequest para gestionar comandos de drones  y la clase CommandsRequest para manejar listas de comandos de movimiento. Implementado el servicio FlightService para ejecutar comandos y validar colisiones.

---
+++ fct/src/main/java/com/drones/fct/dto/BatchDroneCommandRequest.java
@@ -0,0 +1,26 @@
+package com.drones.fct.dto;
+
+import java.util.List;
+
+import com.drones.fct.domain.MovementCommand;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class BatchDroneCommandRequest {
+  @Schema(description = "List of commands to execute by drones")
+  private List<DroneCommand> commands;
+
+  @Data
+  public static class DroneCommand {
+    @NotNull
+    @Schema(description = "Drone identifier")
+    private Long droneId;
+
+    @NotNull
+    @Schema(description = "List of movement commands")
+    private List<MovementCommand> commands;
+  }
+}
