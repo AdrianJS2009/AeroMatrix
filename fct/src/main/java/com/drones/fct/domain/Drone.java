7a5c23e9eea4db1f299a27846b686d7fbdaccbfc
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-03-19 07:34:04 +0100

    Creada y mapeada entidad Matrix, añadidas dependencias; lombok y logback

--- fct/src/main/java/com/drones/fct/domain/Drone.java
+++ fct/src/main/java/com/drones/fct/domain/Drone.java
@@ -2,12 +2,19 @@ package com.drones.fct.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class Drone {
   @Entity
