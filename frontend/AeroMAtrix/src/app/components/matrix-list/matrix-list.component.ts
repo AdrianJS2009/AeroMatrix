cf1431dc4a14f45443bbec3ae6cd53087e2cb9de
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-01 14:36:22 +0200

    Agregar componente MatrixList con plantilla y pruebas unitarias

---
+++ frontend/AeroMAtrix/src/app/components/matrix-list/matrix-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matrix-list',
+  imports: [],
+  templateUrl: './matrix-list.component.html',
+  styleUrl: './matrix-list.component.scss'
+})
+export class MatrixListComponent {
+
+}
