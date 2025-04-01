f390739bb5658ce761fb577514cf46ad2972d93d
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-01 14:36:44 +0200

    Agregar componente MatrixDetail con plantilla, estilos y pruebas unitarias

---
+++ frontend/AeroMAtrix/src/app/components/matrix-detail/matrix-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matrix-detail',
+  imports: [],
+  templateUrl: './matrix-detail.component.html',
+  styleUrl: './matrix-detail.component.scss'
+})
+export class MatrixDetailComponent {
+
+}
