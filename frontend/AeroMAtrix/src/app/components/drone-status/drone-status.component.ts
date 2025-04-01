7f3d95982d5bb1ca9b6b22ab7116c5b877f7368d
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-01 14:38:10 +0200

    Agregar componente DroneStatus con plantilla, estilos y pruebas unitarias

---
+++ frontend/AeroMAtrix/src/app/components/drone-status/drone-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drone-status',
+  imports: [],
+  templateUrl: './drone-status.component.html',
+  styleUrl: './drone-status.component.scss'
+})
+export class DroneStatusComponent {
+
+}
