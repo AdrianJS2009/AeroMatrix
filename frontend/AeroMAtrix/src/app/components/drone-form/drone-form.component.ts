942e077938c2d4b7a4e13f97a975c631a82a1334
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-01 14:37:05 +0200

    Agregar componente DroneForm con plantilla, estilos y pruebas unitarias

---
+++ frontend/AeroMAtrix/src/app/components/drone-form/drone-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drone-form',
+  imports: [],
+  templateUrl: './drone-form.component.html',
+  styleUrl: './drone-form.component.scss'
+})
+export class DroneFormComponent {
+
+}
