171999d0c21222dc885e4de1f472c359cedd1031
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-01 14:37:22 +0200

    Agregar componente DroneList con plantilla y pruebas unitarias

---
+++ frontend/AeroMAtrix/src/app/components/drone-list/drone-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drone-list',
+  imports: [],
+  templateUrl: './drone-list.component.html',
+  styleUrl: './drone-list.component.scss'
+})
+export class DroneListComponent {
+
+}
