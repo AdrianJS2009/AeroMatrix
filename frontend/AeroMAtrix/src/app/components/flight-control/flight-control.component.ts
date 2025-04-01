a49adbf729902d02eb8eab73cbb28c958babef7e
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-01 14:37:42 +0200

    Agregar componente FlightControl con plantilla, estilos y pruebas unitarias

---
+++ frontend/AeroMAtrix/src/app/components/flight-control/flight-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-control',
+  imports: [],
+  templateUrl: './flight-control.component.html',
+  styleUrl: './flight-control.component.scss'
+})
+export class FlightControlComponent {
+
+}
