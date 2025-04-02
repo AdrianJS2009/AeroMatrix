8a3350ac0fc7fdd6db281035383430adb865c2c9
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-02 08:03:17 +0200

    Agregar componentes de formulario y visualización de matrices con pruebas unitarias

---
+++ frontend/AeroMAtrix/src/app/components/matrix-form/matrix-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matrix-form',
+  imports: [],
+  templateUrl: './matrix-form.component.html',
+  styleUrl: './matrix-form.component.scss'
+})
+export class MatrixFormComponent {
+
+}
---
+++ frontend/AeroMAtrix/src/app/components/matrix-visual/matrix-visual.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matrix-visual',
+  imports: [],
+  templateUrl: './matrix-visual.component.html',
+  styleUrl: './matrix-visual.component.scss'
+})
+export class MatrixVisualComponent {
+
+}
