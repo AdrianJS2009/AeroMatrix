1a52390a14bcb240a6c730849dfcbe87b1cf4e43
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-02 07:49:57 +0200

    Agregar rutas para la gestión de drones y control de vuelo en el módulo de enrutamiento

--- frontend/AeroMAtrix/src/app/app-routing.module.ts
+++ frontend/AeroMAtrix/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, type Routes } from '@angular/router';
+import { DroneFormComponent } from './components/drone-form/drone-form.component';
+import { DroneListComponent } from './components/drone-list/drone-list.component';
+import { FlightControlComponent } from './components/flight-control/flight-control.component';
 import { MatrixDetailComponent } from './components/matrix-detail/matrix-detail.component';
 import { MatrixListComponent } from './components/matrix-list/matrix-list.component';
 
@@ -7,7 +10,11 @@ const routes: Routes = [
   { path: '', redirectTo: '/matrices', pathMatch: 'full' },
   { path: 'matrices', component: MatrixListComponent },
   { path: 'matrices/:id', component: MatrixDetailComponent },
-  // Agrega más rutas según sea necesario
+  { path: 'drones', component: DroneListComponent },
+  { path: 'drones/new', component: DroneFormComponent },
+  { path: 'drones/:id/edit', component: DroneFormComponent },
+  { path: 'flight-control', component: FlightControlComponent },
+  { path: '**', redirectTo: '/matrices' },
 ];
 
 @NgModule({
