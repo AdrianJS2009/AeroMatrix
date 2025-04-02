4dd2239e45e0b6fbde13f7c9680691137fd95f79
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-02 07:53:47 +0200

    Eliminar comentario innecesario

--- frontend/AeroMAtrix/src/app/app.component.ts
+++ frontend/AeroMAtrix/src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent {
   constructor(private primengConfig: PrimeNGConfig) {}
 
   ngOnInit() {
-    this.primengConfig.ripple = true; // El efecto ripple es:
+    this.primengConfig.ripple = true;
   }
 }
