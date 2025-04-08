1a1f4c5eb9de55a21ac063636effbedddd34ae73
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-04-08 10:43:00 +0200

    Refactor AboutComponent: remove old implementation and add new structure with enhanced layout, animations, and styling

--- frontend/AeroMAtrix/src/app/config/app.routes.ts
+++ frontend/AeroMAtrix/src/app/config/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { ShellComponent } from '../layout/shell.component';
 
 export const routes: Routes = [
+  // Landing page route for the root path
   {
     path: '',
     component: LandingPageComponent,
     pathMatch: 'full',
   },
+  // Shell route layout for the main application
   {
     path: '',
     component: ShellComponent,
@@ -44,9 +46,7 @@ export const routes: Routes = [
       {
         path: 'about',
         loadComponent: () =>
-          import('../about/components/about.component').then(
-            (m) => m.AboutComponent
-          ),
+          import('../about/about.component').then((m) => m.AboutComponent),
       },
       {
         path: 'features',
@@ -64,6 +64,7 @@ export const routes: Routes = [
       },
     ],
   },
+  // Not found route
   {
     path: '404',
     loadComponent: () =>
@@ -71,6 +72,7 @@ export const routes: Routes = [
         (m) => m.NotFoundComponent
       ),
   },
+  // Redirect any unmatched path to the Not Found page
   {
     path: '**',
     redirectTo: '404',
